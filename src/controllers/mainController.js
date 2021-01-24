3999fc8164ef8e56bf5d300a68ff7ee8da2b6de3
Cristian <cristiannicolas32@hotmail.com>
2021-01-23 21:17:43 -0300

    Mercadoliebre V3

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -8,11 +8,28 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
 	root: (req, res) => {
-		// Do the magic
-	},
+        const saleProducts = products.filter((products)=>{
+            return products.category === 'in-sale'
+        });
+        const visitedProducts = products.filter((products)=>{
+            return products.category === 'visited'
+        });
+        res.render('index',{
+            saleProducts,visitedProducts,toThousand,
+            
+        });
+    },
 	search: (req, res) => {
-		// Do the magic
-	},
+		const buscar = req.query.buscar;
+
+        const resultado = products.filter(producto=>{
+            return producto.name.includes(buscar)
+		})
+		res.render('results',{
+            buscar,
+			products: resultado
+		})
+	}
 };
 
 module.exports = controller;
--- src/routes/products.js
+++ src/routes/products.js
@@ -6,11 +6,11 @@ const router = express.Router();
 const productsController = require('../controllers/productsController');
 
 router.get('/', productsController.root); /* GET - All products */
-router.get('/detail/:productId/', productsController.detail); /* GET - Product detail */
+router.get('/detail/:productId', productsController.detail); /* GET - Product detail */
 
 /*** CREATE ONE PRODUCT ***/ 
-router.get('/create/', productsController.create); /* GET - Form to create */
-router.post('/create/', productsController.store); /* POST - Store in DB */
+router.get('/create', productsController.create); /* GET - Form to create */
+router.post('/create', productsController.store); /* POST - Store in DB */
 
 /*** EDIT ONE PRODUCT ***/ 
 router.get('/edit/:productId', productsController.edit); /* GET - Form to create */
